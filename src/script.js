3ab3889ab25904b4c553c82ed5acb44d15d88953
Bogomil Simitchiev <simitchiev365@abv.bg>
2023-02-21 21:37:52 +0200

    Added style and a little functionality

--- src/script.js
+++ src/script.js
@@ -1,7 +1,14 @@
 const buttonElements = document.querySelectorAll('#buttonsField button');
-
+let validOperatorsAndNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '.'];
+let outputElement = document.querySelector('.outputField');
 buttonElements.forEach(btnElement => {
     btnElement.addEventListener('click', (e) => {
-        
+
+        if (validOperatorsAndNumbers.includes(e.currentTarget.value)) {
+            let text = e.currentTarget.value;
+            console.log(text);
+            outputElement.textContent += text;
+        }
+
     })
 })
