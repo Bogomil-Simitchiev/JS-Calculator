a15a5f8c44161f5e3cf0aa8d7f466c7613b978cd
Bogomil Simitchiev <simitchiev365@abv.bg>
2023-02-21 20:29:21 +0200

    Upgraded style and added buttons

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const buttonElements = document.querySelectorAll('#buttonsField button');
+
+buttonElements.forEach(btnElement => {
+    btnElement.addEventListener('click', (e) => {
+        
+    })
+})
