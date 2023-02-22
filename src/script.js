4f0a03798213e767fd98280b0a725780a6645f30
Bogomil Simitchiev <simitchiev365@abv.bg>
2023-02-22 20:48:10 +0200

    Added final functionalities to the project

--- src/script.js
+++ src/script.js
@@ -1,14 +1,39 @@
 const buttonElements = document.querySelectorAll('#buttonsField button');
 let validOperatorsAndNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '.'];
 let outputElement = document.querySelector('.outputField');
+outputElement.textContent = '';
+
+let secondTimeIteration = false;
 buttonElements.forEach(btnElement => {
     btnElement.addEventListener('click', (e) => {
 
         if (validOperatorsAndNumbers.includes(e.currentTarget.value)) {
             let text = e.currentTarget.value;
-            console.log(text);
             outputElement.textContent += text;
         }
+        if (e.currentTarget.value == '=') {
+            if (outputElement.textContent[0] == '.') {
+                alert('Error! Please check your input');
+                outputElement.textContent = '';
+                return;
+            }
+            try {
+                let result = eval(outputElement.textContent).toFixed(2);
+                outputElement.textContent = result;
+            } catch (error) {
+                alert('Error! Please check your input');
+                outputElement.textContent = '';
+            }
+
+        }
+        if (e.currentTarget.value == 'all-clear') {
+            outputElement.textContent = '';
+        }
+        if (outputElement.textContent.length == 23) {
+            alert('Too big operation or number, please enter smaller one!');
+            outputElement.textContent = '';
+        }
+
 
     })
 })
