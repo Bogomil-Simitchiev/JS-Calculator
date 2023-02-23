ac1fb7e5b0446c01e1c7ee02264b8034a8a252f9
Bogomil Simitchiev <simitchiev365@abv.bg>
2023-02-23 17:41:01 +0200

    Created logic of button 'CE' and added final functionalities to the project

--- src/script.js
+++ src/script.js
@@ -1,12 +1,10 @@
 const buttonElements = document.querySelectorAll('#buttonsField button');
-let validOperatorsAndNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '.'];
+let validOperatorsAndNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '.', '(', ')'];
 let outputElement = document.querySelector('.outputField');
 outputElement.textContent = '';
 
-let secondTimeIteration = false;
 buttonElements.forEach(btnElement => {
     btnElement.addEventListener('click', (e) => {
-
 
         if (validOperatorsAndNumbers.includes(e.currentTarget.value)) {
             if (outputElement.textContent.length > 0 && ((outputElement.textContent[outputElement.textContent.length - 1] == '-') && (e.currentTarget.value == '-'))) {
@@ -28,15 +26,15 @@ buttonElements.forEach(btnElement => {
         }
         if (e.currentTarget.value == '=') {
             if (outputElement.textContent[0] == '.') {
-                alert('Error! Please check your input');
+                alert('Error! Please check your input field!');
                 outputElement.textContent = '';
                 return;
             }
             try {
-                let result = eval(outputElement.textContent).toFixed(2);
+                let result = eval(outputElement.textContent);
                 outputElement.textContent = result;
             } catch (error) {
-                alert('Error! Please check your input');
+                alert('Error! Please check your input field!');
                 outputElement.textContent = '';
             }
 
@@ -44,11 +42,19 @@ buttonElements.forEach(btnElement => {
         if (e.currentTarget.value == 'all-clear') {
             outputElement.textContent = '';
         }
-        if (outputElement.textContent.length == 23) {
+        if (outputElement.textContent.length == 24) {
             alert('Too big operation or number, please enter smaller one!');
             outputElement.textContent = '';
         }
-        //create functionality to button CE:
+        if (e.currentTarget.value == 'back') {
+            if (outputElement.textContent.length > 0) {
+                let result = outputElement.textContent.substring(0, outputElement.textContent.length - 1);
+                outputElement.textContent = result;
+
+            } else {
+                alert('Input field is empty!');
+            }
+        }
 
     })
 })
